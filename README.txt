8a17b86b2a3993e936811743cf035707ae431baf
jamaurz <mazurhalinapost@gmail.com>
2017-08-16 22:48:25 +0300

    init commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+To run the demo
+    python -m http.server 8000
---
+++ lesson_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from main import *
+
+class SumTests(unittest.TestCase):
+    def test_main(self):
+        self.assertIsInstance(total, int)
+        self.assertEqual(total, 55)
+
+if __name__=='__main__':
+	SumTests().test_main()
